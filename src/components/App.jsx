5ddb10785daeb7831acfa6e3ba1c3d2a4400757c
MariaKhmel <mariakhmel8@gmail.com>
2024-02-12 22:08:53 +0200

    starting files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,74 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import {ContactsList} from './ContactsList/ContactsList'
+import {Filter} from './Filter/Filter'
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+
+state = {
+contacts: [],
+filter: '',
+}
+
+handleFormChange = (data) => {
+    const newContact = {
+      id: nanoid(),
+      ...data,
+  }
+  const newContactName = data.name;
+  if (this.isContactinList(newContactName)) {
+    alert(`${newContactName} is already in contacts.`)
+    return; 
+  }
+    this.setState(prevstate => {
+    const newContactsList = [...prevstate.contacts,newContact];
+      return {
+        contacts: newContactsList,
+      }
+    })
+  }
+ 
+  handleFilterChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+     [name]:value,
+   })
+}
+ 
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const lowercaseFilter = filter.toLowerCase();
+    return contacts.filter(contact => (
+      contact.name.toLowerCase().includes(lowercaseFilter)
+    ))
+    }
+  
+  isContactinList = (newName) => {
+    const { contacts } = this.state;
+    const lowercaseName = newName.toLowerCase();
+    return contacts.find(({name})=>name.toLowerCase()===lowercaseName )
+  }
+  
+  deleteContact = id => {
+   
+    this.setState(prevstate => ({
+      contacts:prevstate.contacts.filter(contact=> contact.id !==id)
+    }))
+}
+
+  render(){
+    const filteredContacts = this.getFilteredContacts();
+     
+    return (
+      <div style={{padding:'10px' }}>
+        <h1>Phonebook</h1>
+        <ContactForm handleFormChange={this.handleFormChange} />
+        <Filter handleFilterChange={this.handleFilterChange}
+          filter={this.state.filter} />
+        <h2>Contacts</h2>
+        <ContactsList contactlist={filteredContacts} onClick={this.deleteContact} />
+      </div>
+    );
+  }
+}
